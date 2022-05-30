ea0223f700c38753998f42699016b72095aaaf41
Samky444 <samkiape44@gmail.com>
2022-05-29 20:10:03 -0700

    Add project files.

---
+++ InventoryMaintenance/InvItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryMaintenance
+{
+    public class InvItem
+    {
+        private int itemNo;
+        private string description;
+        private decimal price;
+        public InvItem()
+        {
+        }
+        // Constructor that initializes the object
+
+        public InvItem(int itemNo, string description, decimal price)
+        {
+            this.itemNo = itemNo;
+            this.description = description;
+            this.price = price;
+        }
+        // properties
+        public int ItemNo
+        {
+            get
+            {
+                return itemNo;
+            }
+            set
+            {
+                itemNo = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+        public decimal Price  // property
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+            public string GetDisplayText() => itemNo + "    " + description + " (" + price.ToString("c") + ")";
+        }
+
+    }          
+
---
+++ InventoryMaintenance/frmNewItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventoryMaintenance
+{
+    public partial class frmNewItem : Form
+    {
+        public frmNewItem()
+        {
+            InitializeComponent();
+        }
+        // Add a statement here that declares the inventory item.         
+
+        private InvItem invItem = null;  // initial value of null
+        public InvItem GetNewItem() // public method that displays the form as a dialog box
+        {
+            this.ShowDialog();
+            return invItem; // Add a method here that gets and returns a new item.
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+                invItem = new InvItem(Convert.ToInt32(txtItemNo.Text),
+                 txtDescription.Text, Convert.ToDecimal(txtPrice.Text));
+                this.Close();
+            }
+        }
+
+        private bool IsValidData()
+        {
+            return Validator.IsPresent(txtItemNo) &&
+                   Validator.IsInt32(txtItemNo) &&
+                   Validator.IsPresent(txtDescription) &&
+                   Validator.IsPresent(txtPrice) &&
+                   Validator.IsDecimal(txtPrice);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmNewItem_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
